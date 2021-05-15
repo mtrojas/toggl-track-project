6197555e2d8e9fecc84429c81ecfdee7ae5ec478
MT <mtrojas@protonmail.com>
2021-05-15 12:59:48 -0400

    feat: add main and variables terraform files

---
+++ infrastructure/variables.tf
@@ -0,0 +1,16 @@
+variable "region" {
+  default     = "southamerica-east1"
+  description = "The region for subnetworks in the network"
+  type        = string
+}
+
+variable "project" {
+  default     = "toggl-track-project"
+  description = "The project ID for the network"
+  type        = string
+}
+
+
+
+
+
