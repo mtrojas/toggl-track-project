9a5b059ad8c64fece1c4ffe0ebff18cfbeba47f1
MT <mtrojas@protonmail.com>
2021-05-18 18:01:30 -0400

    feat: add backend and use service account credentials

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,4 +1,19 @@
 provider "google" {
-  project = var.project
-  region  = var.region
+  credentials = file("/Users/mtrojas/Downloads/terraform-key.json")
+  project     = var.project
+  region      = var.region
+  zone        = var.zone
 }
+
+terraform {
+  backend "gcs" {
+    bucket      = "toggl-track-project-tfstate"
+    prefix      = "tfstate"
+    credentials = "/Users/mtrojas/Downloads/terraform-key.json"
+
+  }
+}
+
+
+
+
