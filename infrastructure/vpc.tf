2f45ae9c1e6f6174bf7633fc92bd6f2e6d44d99c
MT <mtrojas@protonmail.com>
2021-05-21 19:04:10 -0400

    feat: create private ip and use it for db

--- infrastructure/vpc.tf
+++ infrastructure/vpc.tf
@@ -11,6 +11,21 @@ resource "google_compute_subnetwork" "public_subnetwork" {
   network       = google_compute_network.vpc_network.name
 }
 
+resource "google_compute_global_address" "private_ip_block" {
+  name          = "private-ip-block"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  ip_version    = "IPV4"
+  prefix_length = 20
+  network       = google_compute_network.vpc_network.self_link
+}
+
+resource "google_service_networking_connection" "private_vpc_connection" {
+  network                 = google_compute_network.vpc_network.self_link
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_block.name]
+}
+
 resource "google_compute_firewall" "fw_web" {
   project = var.project
   name    = "fw-web"
