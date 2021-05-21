4daeea0e67a11de4934e167acb886352984ccf41
MT <mtrojas@protonmail.com>
2021-05-20 20:08:21 -0400

    chore: cleanup

--- infrastructure/vpc.tf
+++ infrastructure/vpc.tf
@@ -4,9 +4,6 @@ resource "google_compute_network" "vpc_network" {
   auto_create_subnetworks = false
 }
 
-//firewall
-// internet gateway?
-
 resource "google_compute_subnetwork" "public_subnetwork" {
   name          = "public-sbn"
   ip_cidr_range = "10.2.0.0/16"
@@ -30,13 +27,3 @@ resource "google_compute_firewall" "fw_web" {
   source_tags   = ["web"]
   source_ranges = ["0.0.0.0/0"]
 }
-
-//https://registry.terraform.io/modules/gruntwork-io/network/google/latest/submodules/vpc-network
-# module "network_vpc-network" {
-#   source  = "gruntwork-io/network/google//modules/vpc-network"
-#   version = "0.8.1"
-#   # insert the 5 required variables here
-#   project     = var.project
-#   name_prefix = var.name_prefix
-#   region      = var.region
-# }
