a5aa77c857664a2b32a757eb0a425622d724ab38
MT <mtrojas@protonmail.com>
2021-05-20 20:05:29 -0400

    chore: improve startup script

--- infrastructure/startup.sh
+++ infrastructure/startup.sh
@@ -3,5 +3,5 @@ apt-get update && apt update && apt-get upgrade -y && apt upgrade -y && apt-get 
 apt-get autoclean && apt autoclean && apt-get clean && apt clean && apt-get autoremove --purge -y && apt autoremove --purge -y
 wget https://golang.org/dl/go1.16.4.linux-amd64.tar.gz
 tar -xzvf go1.16.4.linux-amd64.tar.gz -C /usr/local
-
+export PATH=$PATH:/usr/local/go/bin >> /etc/profile
 reboot
