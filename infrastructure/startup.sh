4f1d02dca2d9c0077c32cb2362215be311d98c39
MT <mtrojas@protonmail.com>
2021-05-23 14:19:35 -0400

    chore: tweak the startup script

--- infrastructure/startup.sh
+++ infrastructure/startup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#! /bin/bash
 apt-get update && apt update && apt-get upgrade -y && apt upgrade -y && apt-get dist-upgrade -y && apt dist-upgrade -y
 apt-get autoclean && apt autoclean && apt-get clean && apt clean && apt-get autoremove --purge -y && apt autoremove --purge -y
 wget https://golang.org/dl/go1.16.4.linux-amd64.tar.gz
